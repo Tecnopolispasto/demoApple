905e9a8225b1388b2784a1d5e32810c250a73af0
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-03-22 22:58:22 -0600

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-
+import { appleImg, bagImg, searchImg } from "../../public/utils";
+import { navLists } from "../constants/index";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="Apple" width={14} height={18} />
+      </nav>
+
+      <div className="flex flex-1 justify-center max-sm:hidden">
+        {navLists.map((nav) => (
+          <div key={nav}>{nav}</div>
+        ))}
+      </div>
+
+      <div>
+        <img src={searchImg} alt="Search" width={18} height={18} />
+        <img src={bagImg} alt="bag" width={18} height={18} />
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/constants/index.js
+++ src/constants/index.js
@@ -7,7 +7,7 @@ import {
   highlightThirdVideo,
   whiteImg,
   yellowImg,
-} from "../utils";
+} from "../../public/utils";
 
 export const navLists = ["Store", "Mac", "iPhone", "Support"];
 
